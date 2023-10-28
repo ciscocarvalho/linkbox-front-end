6b4189a63642fdc6565fc236d996fc59653a343e
Francisco <francisco.carvalho.q@gmail.com>
2023-10-28 18:20:23 -0300

    refactor: substituindo CSS por TailwindCSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from 'next';
 import React from 'react';
 import './reset.css';
-import './style.css';
 
 export const metadata: Metadata = {
   icons: [{ rel: 'short icon', url: 'images/Logo.svg' } ],
@@ -9,32 +8,38 @@ export const metadata: Metadata = {
 }
 
 const page: React.FC = () => {
+
   return <>
     <script src="scripts/main.js" defer></script>
-    <header>
-        <a href="/">
-            <div className="brand">
-                <img className="logo" src="images/Logo.svg" alt="LinkBox logo" />
-                <p className="website-name">LinkBox</p>
+    <div className="bg-[#f4f4f4] flex justify-center items-center flex-col h-[100vh]">
+        <header className="w-[80%] h-[10%] flex justify-between items-center mb-[30px] max-[500px]:mb-0 max-[500px]:w-[100%]">
+            <a className="max-[500px]:text-[20px]" href="/">
+                <div className="flex items-center">
+                <img className="w-[48px] h-[48px] mr-[10px] max-[500px]:ml-[10px]" src="images/Logo.svg" alt="LinkBox logo" />
+                    <p className="text-[2rem] font-bold max-[310px]:text-[30px] max-[280px]:text-[20px]">LinkBox</p>
+                </div>
+            </a>
+            <nav>
+            <li className="m-[5px]"><a href="login" className="max-[500px]:text-[20px] border-b-[2px] border-solid border-[#00000000] duration-[.2s] hover:duration-[.2s] hover:border-b-[black]">Entrar</a></li>
+                <li className="m-[5px]"><a className="max-[500px]:text-[20px]" href="cadastro"><span className="bg-[#90CDF4] p-[15px] rounded-[10px] duration-[.2s] hover:duration-[.2s] hover:bg-[#52a2d4] max-[500px]:hidden">Começe agora</span></a></li>
+
+            </nav>
+        </header>
+        <section className="flex w-[100%] h-[90%] justify-around items-center max-[500px]:flex-col max-[500px]:justify-center max-[500px]:items-center">
+        <div className="flex flex-col justify-center items-center ml-[100px] w-[50%] gap-[20px] max-[950px]:ml-[20px] max-[653px]:w-fit max-[500px]:w-auto max-[500px]:ml-0 max-[500px]:mt-0">
+            <p className="text-[72px] text-center font-bold max-[950px]:text-[20px] max-[500px]:hidden max-[500px]:text-[30px] max-[500px]:w-[100%] max-[500px]:h-[250px] max-[280px]:text-[20px] max-[280px]:h-auto">O melhor lugar para organizar seus links!</p>
+                <p className="text-[30px] max-[950px]:text-[20px] max-[500px]:hidden">Use o LinkBox para uma organização sem dor de cabeça</p>
+                <a className="max-[500px]:text-[20px]" href="cadastro">
+                    <span className="bg-[#90CDF4] p-[15px] rounded-[10px] duration-[.2s] hover:duration-[.2s] hover:bg-[#52a2d4] max-[500px]:text-[20px] max-[500px]:p-[20px] max-[500px]:px-[60px] max-[500px]:px-[60px] max-[500px]:mb-[30px] max-[280px]:p-[15px]">Começe agora</span>
+                </a>
             </div>
-        </a>
-        <nav>
-            <li><a href="login" className="login-text">Entrar</a></li>
-            <li><a href="cadastro"><span className="top">Começe agora</span></a></li>
-        </nav>
-    </header>
-    <section>
-        <div className="texto">
-            <p className="headline">O melhor lugar para organizar seus links!</p>
-            <p>Use o LinkBox para uma organização sem dor de cabeça</p>
-            <a href="cadastro"><span>Começe agora</span></a>
-        </div>
-        <div className="img">
-            <img src="images/links.svg" />
-        </div>
-        
-        <a href="login" className="blue-text">Já possui conta? Entrar</a>
-    </section>
+            <div className="max-[653px]:hidden">
+                <img className="w-[500px] max-[950px]:w-[350px]" src="images/links.svg" />
+            </div>
+            
+            <a href="login" className="hidden max-[500px]:text-[20px] max-[500px]:flex max-[500px]:mb-[20px] max-[500px]:text-[#52a2d4]">Já possui conta? Entrar</a>
+        </section>
+    </div>
   </>
 };
 
