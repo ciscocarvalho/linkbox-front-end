3faf7b2ea9f205a15f52e143344b7e424358e1b0
Francisco <francisco.carvalho.q@gmail.com>
2023-10-29 19:39:50 -0300

    refactor(serverError): substituindo CSS por TailwindCSS

--- src/app/serverError/page.tsx
+++ src/app/serverError/page.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from 'next';
 import React from 'react';
 import '../reset.css';
-import './serverError.css';
 
 export const metadata: Metadata = {
   title: 'Server Down',
@@ -9,16 +8,16 @@ export const metadata: Metadata = {
 }
 
 const page: React.FC = () => {
-  return <>
-    <header>
-        <img src="images/Logo.svg" className="logo" />
-        <p className="website-name">LinkBox</p>
+  return <div className="bg-[#2795DB] flex justify-center items-center flex-col h-[100%]">
+    <header className="flex mt-[50px] mb-[30px]">
+        <img src="images/Logo.svg" className="w-[80px] h-[80px]" />
+        <p className="w-[224px] h-[75px] text-[64px] leading-[75px] font-bold">LinkBox</p>
     </header>
-    <section>
-        <img src="images/server_down.svg" className="erro" />
-        <p className="error-message">Ocorreu algum erro no servidor!</p>
+    <section className="flex flex-col justify-between items-center">
+        <img src="images/server_down.svg" className="w-[858.4px] h-[470px]" />
+        <p className="font-bold text-[48px] leading-[56px] mt-[20px]">Ocorreu algum erro no servidor!</p>
     </section>
-  </>
+  </div>
 };
 
 export default page;
