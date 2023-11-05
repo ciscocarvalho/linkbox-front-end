1ef19752cc951eeb93490cb580e26db975951d99
Francisco <francisco.carvalho.q@gmail.com>
2023-11-05 13:52:37 -0300

    refactor(dashboardReducer): movendo findInDashboard para arquivo proprio

--- src/app/dashboard/util/dashboardReducer.ts
+++ src/app/dashboard/util/dashboardReducer.ts
@@ -3,6 +3,7 @@ import DashboardItem from "../DashboardItem";
 import DashboardLink from "../DashboardLink";
 import { DashboardAction, DashboardItemID, TDashboard } from "../types";
 import { mapItemsToItemIDs } from "../util";
+import { findInDashboard } from "./findInDashboard";
 
 type Behavior = "inclusive" | "exclusive";
 
@@ -29,92 +30,6 @@ const copyItemsToFolder = (items: DashboardItem[], folder: DashboardFolder) => {
   const copiedItems = items.map(item => item.clone());
   copiedItems.forEach(item => cutItemsToFolder([item], folder));
   return copiedItems;
-}
-
-const traverseUpFolder = (folder: DashboardFolder, fn: (folder: DashboardFolder) => boolean | void) => {
-  let currentFolder = folder;
-
-  while (true) {
-    const stopTraversal = fn(currentFolder);
-
-    if (stopTraversal) {
-      break;
-    }
-
-    const parent = currentFolder.getParent();
-
-    if (parent) {
-      currentFolder = parent;
-    } else {
-      break;
-    }
-  }
-}
-
-const traverseDownFolder = (folder: DashboardFolder, fn: (folder: DashboardFolder) => boolean | void) => {
-  let currentFolder = folder;
-
-  const stopTraversal = fn(currentFolder);
-
-  if (stopTraversal) {
-    return;
-  }
-
-  const children = currentFolder.getChildren();
-  const subfolders = children.filter((item: DashboardItem) => item instanceof DashboardFolder);
-
-  for (let subfolder of subfolders as DashboardFolder[]) {
-    traverseDownFolder(subfolder, fn);
-  }
-}
-
-const getDashboardRoot = (dashboard: TDashboard) => {
-  let root = dashboard.currentFolder;
-
-  traverseUpFolder(root, subFolder => {
-    const parent = subFolder.getParent();
-    if (parent) {
-      root = parent;
-    } else {
-      return true;
-    }
-  })
-
-  return root;
-}
-
-const traverseDashboard = (dashboard: TDashboard, fn: (folder: DashboardFolder) => boolean | void) => {
-  const root = getDashboardRoot(dashboard);
-  traverseDownFolder(root, fn);
-}
-
-const isRoot = (folder: DashboardFolder) => {
-  return folder.getParent() === null;
-}
-
-const findInDashboard = (dashboard: TDashboard, predicate: Function) => {
-  let result: DashboardItem | null = null;
-
-  traverseDashboard(dashboard, (folder) => {
-    let item;
-
-    if (isRoot(folder)) {
-      item = [folder].find((item) => predicate(item));
-
-      if (item) {
-        result = item;
-        return;
-      }
-    }
-
-    item = folder.getChildren().find((item) => predicate(item));
-
-    if (item) {
-      result = item;
-    }
-  });
-
-  return result as DashboardItem | null;
 }
 
 const updateDashboard = (oldDashboard: TDashboard, updater: (newDashboard: TDashboard) => TDashboard) => {
---
+++ src/app/dashboard/util/findInDashboard.ts
@@ -0,0 +1,90 @@
+import DashboardFolder from "../DashboardFolder";
+import DashboardItem from "../DashboardItem";
+import { TDashboard } from "../types";
+
+const traverseUpFolder = (folder: DashboardFolder, fn: (folder: DashboardFolder) => boolean | void) => {
+  let currentFolder = folder;
+
+  while (true) {
+    const stopTraversal = fn(currentFolder);
+
+    if (stopTraversal) {
+      break;
+    }
+
+    const parent = currentFolder.getParent();
+
+    if (parent) {
+      currentFolder = parent;
+    } else {
+      break;
+    }
+  }
+}
+
+const traverseDownFolder = (folder: DashboardFolder, fn: (folder: DashboardFolder) => boolean | void) => {
+  let currentFolder = folder;
+
+  const stopTraversal = fn(currentFolder);
+
+  if (stopTraversal) {
+    return;
+  }
+
+  const children = currentFolder.getChildren();
+  const subfolders = children.filter((item: DashboardItem) => item instanceof DashboardFolder);
+
+  for (let subfolder of subfolders as DashboardFolder[]) {
+    traverseDownFolder(subfolder, fn);
+  }
+}
+
+const getDashboardRoot = (dashboard: TDashboard) => {
+  let root = dashboard.currentFolder;
+
+  traverseUpFolder(root, subFolder => {
+    const parent = subFolder.getParent();
+    if (parent) {
+      root = parent;
+    } else {
+      return true;
+    }
+  })
+
+  return root;
+}
+
+
+const traverseDashboard = (dashboard: TDashboard, fn: (folder: DashboardFolder) => boolean | void) => {
+  const root = getDashboardRoot(dashboard);
+  traverseDownFolder(root, fn);
+}
+
+const isRoot = (folder: DashboardFolder) => {
+  return folder.getParent() === null;
+}
+
+export const findInDashboard = (dashboard: TDashboard, predicate: Function) => {
+  let result: DashboardItem | null = null;
+
+  traverseDashboard(dashboard, (folder) => {
+    let item;
+
+    if (isRoot(folder)) {
+      item = [folder].find((item) => predicate(item));
+
+      if (item) {
+        result = item;
+        return;
+      }
+    }
+
+    item = folder.getChildren().find((item) => predicate(item));
+
+    if (item) {
+      result = item;
+    }
+  });
+
+  return result as DashboardItem | null;
+}
