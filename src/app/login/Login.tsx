4ab7b0e1214c0578d1e72a4b1eabcf8a59cd8d7a
Francisco <francisco.carvalho.q@gmail.com>
2023-11-16 04:11:13 -0300

    feat(login): redirecionamento automatico para dashboard se usuario ja estiver logado

---
+++ src/app/login/Login.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React from 'react';
+import LoginForm from './LoginForm';
+import GooglePrimaryButton from '../components/GooglePrimaryButton';
+import { useCookies } from 'react-cookie';
+
+const Login: React.FC = () => {
+  const [cookies, _] = useCookies(["token"]);
+
+  if (cookies.token) {
+    window.location.href = "/dashboard";
+    return null;
+  }
+
+  return <div className="bg-[#2795DB] flex items-center justify-center w-[100%] h-[100vh] gap-[80px] max-[445px]:bg-[#ffffff] max-[445px]:h-fit">
+    <img src="/images/side-image.svg" className="w-[50%] max-[1060px]:hidden" />
+    <div className="w-[50%] bg-[#ffffff] flex justify-center items-center flex-col rounded-[40px] h-fit my-[60px] mr-[50px] p-[70px] gap-[30px] max-[1060px]:w-[80%] max-[1060px]:mr-0 max-[445px]:w-[100%] max-[445px]:mt-0 max-[445px]:mb-0 max-[445px]:max-h-fit max-[445px]:gap-[100px]">
+        <a href="/">
+            <div className="flex justify-center items-center gap-[10px] font-light text-[30px]">
+                <img className="max-[445px]:w-[40px]" src="/images/Logo.svg" alt="LinkBox logo" />
+                <p className="text-[2em] font-bold max-[445px]:text-[24px]">LinkBox</p>
+            </div>
+        </a>
+        <LoginForm />
+        <div className="flex flex-col">
+            <div className="max-[445px]:hidden">
+                <GooglePrimaryButton>
+                    <img src="/images/Google logo.svg" />
+                    <p>Continuar com o Google</p>
+                </GooglePrimaryButton>
+            </div>
+            <div className="flex w-[279px] h-[60px] justify-center items-center">
+                <a href="/cadastro" className="text-[#2795DB]">
+                    Ainda não possui conta? Cadastrar
+                </a>
+            </div>
+        </div>
+    </div>
+  </div>
+};
+
+export default Login;
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,7 @@
 import { Metadata } from 'next';
 import React from 'react';
 import '../reset.css';
-import LoginForm from './LoginForm';
-import GooglePrimaryButton from '../components/GooglePrimaryButton';
+import Login from './Login';
 
 export const metadata: Metadata = {
   title: 'Login',
@@ -10,31 +9,7 @@ export const metadata: Metadata = {
 }
 
 const Page: React.FC = () => {
-  return <div className="bg-[#2795DB] flex items-center justify-center w-[100%] h-[100vh] gap-[80px] max-[445px]:bg-[#ffffff] max-[445px]:h-fit">
-    <img src="/images/side-image.svg" className="w-[50%] max-[1060px]:hidden" />
-    <div className="w-[50%] bg-[#ffffff] flex justify-center items-center flex-col rounded-[40px] h-fit my-[60px] mr-[50px] p-[70px] gap-[30px] max-[1060px]:w-[80%] max-[1060px]:mr-0 max-[445px]:w-[100%] max-[445px]:mt-0 max-[445px]:mb-0 max-[445px]:max-h-fit max-[445px]:gap-[100px]">
-        <a href="/">
-            <div className="flex justify-center items-center gap-[10px] font-light text-[30px]">
-                <img className="max-[445px]:w-[40px]" src="/images/Logo.svg" alt="LinkBox logo" />
-                <p className="text-[2em] font-bold max-[445px]:text-[24px]">LinkBox</p>
-            </div>
-        </a>
-        <LoginForm />
-        <div className="flex flex-col">
-            <div className="max-[445px]:hidden">
-                <GooglePrimaryButton>
-                    <img src="/images/Google logo.svg" />
-                    <p>Continuar com o Google</p>
-                </GooglePrimaryButton>
-            </div>
-            <div className="flex w-[279px] h-[60px] justify-center items-center">
-                <a href="/cadastro" className="text-[#2795DB]">
-                    Ainda não possui conta? Cadastrar
-                </a>
-            </div>
-        </div>
-    </div>
-  </div>
+  return <Login />
 };
 
 export default Page;
