586a107608f66ef90eb4051012bc055a6a8c61a5
Francisco <francisco.carvalho.q@gmail.com>
2023-11-24 11:26:13 -0300

    fix(not-found): consertando responsividade

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,19 +8,19 @@ export const metadata: Metadata = {
 }
 
 const page: React.FC = () => {
-  return <div className="bg-[#2795DB] flex justify-center items-center flex-col h-[100vh] gap-[50px]">
+  return <div className="bg-[#2795DB] flex justify-center items-center flex-col min-h-[inherit] gap-[50px] p-[5px]">
     <a href="/">
-        <div className="flex gap-[20px]">
-            <img src="/images/Logo.svg" className="w-[80px] h-[80px]" />
-            <p className="font-bold w-[224px] h-[75px] text-[64px] leading-[75px]">LinkBox</p>
+        <div className="flex items-center gap-[20px] w-fit">
+            <img src="/images/Logo.svg" className="w-[80px] max-[540px]:w-[60px]" />
+            <p className="font-bold text-[48px] max-[540px]:text-[36px]">LinkBox</p>
         </div>
     </a>
 
     <section className="flex flex-col justify-between items-center">
-        <img src="/images/erro 404 img.svg" className="select-none w-[858.4px] h-[470px]" />
+        <img src="/images/erro 404 img.svg" className="select-none max-w-[500px] w-[90%]" />
     </section>
 
-    <p className="text-[2rem] font-bold">Página não encontrada!</p>
+    <p className="font-bold text-[clamp(20px,8vw,40px)] text-center">Página não encontrada!</p>
   </div>
 };
 
