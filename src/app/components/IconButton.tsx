be262595c8b86776c6bf21264edd27e85a167074
Francisco <francisco.carvalho.q@gmail.com>
2023-12-01 17:00:48 -0300

    feat(dashboard): adicionando cor de fundo para botoes no evento `active`

--- src/app/components/IconButton.tsx
+++ src/app/components/IconButton.tsx
@@ -15,7 +15,7 @@ const Button: React.FC<ButtonProps> = ({
   return (
     <div
       {...props}
-      className={`min-w-[48px] w-[48px] h-[48px] flex justify-center items-center rounded-[50%] duration-[0.2s] select-none hover:cursor-pointer hover:bg-[#c2c8d1] hover:duration-[0.2s]${props.className ? " " + props.className : ""}`}
+      className={`min-w-[48px] w-[48px] h-[48px] flex justify-center items-center rounded-[50%] duration-[0.2s] select-none hover:cursor-pointer hover:bg-[#c2c8d1] hover:duration-[0.2s] active:bg-[#969da8] ${props.className ? " " + props.className : ""}`}
     >
       <Icon name={icon} />
       {children}
