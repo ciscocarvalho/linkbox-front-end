f123a3266c0f8580d366c2f2cf9d4517e52a6b4a
Francisco <francisco.carvalho.q@gmail.com>
2023-11-30 21:39:03 -0300

    chore: atualizando rota `folderIds` para `foldersFromPaths`

--- src/app/dashboard/util/actions/getFolderByPath.ts
+++ src/app/dashboard/util/actions/getFolderByPath.ts
@@ -1,6 +1,6 @@
 import fetchJsonPayload from "../../../../Services/fetchJsonPayload";
 
 export const getFolderByPath = async (path: string) => {
-  const payload = await fetchJsonPayload("get", `/folderIds/default/${path}`);
+  const payload = await fetchJsonPayload("get", `/foldersFromPaths/default/${path}`);
   return payload?.data ?? null;
 };
