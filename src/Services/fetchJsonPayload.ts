7eee458b15eee03735f0708d723be68909c34cb6
Francisco <francisco.carvalho.q@gmail.com>
2023-11-30 23:10:00 -0300

    refactor: removendo console.log

--- src/Services/fetchJsonPayload.ts
+++ src/Services/fetchJsonPayload.ts
@@ -1,7 +1,5 @@
 import { BACKEND_URL } from "../constants";
 
-let amountOfRequests = 0;
-
 const fetchJsonPayload = async (method: string, route: string, bodyOptions?: object) => {
   const res = await fetch(`${BACKEND_URL}${route}`, {
     method,
@@ -9,9 +7,6 @@ const fetchJsonPayload = async (method: string, route: string, bodyOptions?: obj
     credentials: "include",
     body: bodyOptions ? JSON.stringify(bodyOptions) : undefined,
   });
-
-  amountOfRequests++;
-  console.log("amount of requests:", amountOfRequests);
 
   return await res.json();
 }
