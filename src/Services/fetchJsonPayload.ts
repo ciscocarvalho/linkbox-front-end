b3f28ba7f68729aea8d38deb691e9af7c31b0a85
Francisco <francisco.carvalho.q@gmail.com>
2023-12-01 22:28:13 -0300

    fix: removendo token quando ocorre erro de usuario nao autenticado

--- src/Services/fetchJsonPayload.ts
+++ src/Services/fetchJsonPayload.ts
@@ -1,4 +1,9 @@
+import { Cookies } from "react-cookie";
 import { BACKEND_URL } from "../constants";
+
+const checkUserAuthenticated = (payload: any) => {
+  return payload.error?.message !== "No current user authenticated";
+}
 
 const fetchJsonPayload = async (method: string, route: string, bodyOptions?: object) => {
   const res = await fetch(`${BACKEND_URL}${route}`, {
@@ -8,7 +13,13 @@ const fetchJsonPayload = async (method: string, route: string, bodyOptions?: obj
     body: bodyOptions ? JSON.stringify(bodyOptions) : undefined,
   });
 
-  return await res.json();
+  const payload = await res.json();
+
+  if (!checkUserAuthenticated(payload)) {
+    new Cookies().remove("token");
+  }
+
+  return payload;
 }
 
 export default fetchJsonPayload;
